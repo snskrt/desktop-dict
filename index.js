5e90f61995a05d55b11884dceef1d15a180ce188
Dan Kocherga <dan.kocherga@gmail.com>
2017-02-19 17:53:53 +0300

    Fix copy/paste support

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import api from './main_process/Api'
 
 const app = electron.app;
 const browserWindow = electron.BrowserWindow;
+const menu = electron.Menu;
 
 api.listen(electron.ipcMain);
 
@@ -16,6 +17,29 @@ function createWindow() {
     win.on('closed', function () {
         win = null;
     });
+
+    menu.setApplicationMenu(menu.buildFromTemplate(createMenu()));
+}
+
+function createMenu() {
+    return [
+        {
+            label: "Application",
+            submenu: [
+                { label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
+                { type: 'separator' },
+                { label: 'Quit', accelerator: 'Command+Q', click: function() { app.quit(); }}
+            ]
+        },
+        {
+            label: 'Edit',
+            submenu: [
+                { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+                { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+                { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+            ]
+        }
+    ];
 }
 
 app.on('ready', createWindow);
