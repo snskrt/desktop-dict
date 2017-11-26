22d43e27e9114e270d26013c58509aa4511915e9
Dan Kocherga <dan.kocherga@gmail.com>
2017-11-26 16:49:15 +0300

    Remove uglify.js

--- webpack.config.js
+++ webpack.config.js
@@ -20,13 +20,6 @@ const config = {
                 'NODE_ENV': JSON.stringify('production')
             }
         }),
-        // Minify the bundle
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                // suppresses warnings, usually from module minification
-                warnings: false,
-            },
-        }),
         // Allows error warnings but does not stop compiling.
         new webpack.NoErrorsPlugin(),
         // Transfer Files
